eedc30d4694fb86c9d725ceb9a9a76045055a222
BionicHawk <angel.manzo.rosas@gmail.com>
2024-04-15 16:49:13 -0600

    Agregué el encabezado de la aplicación, no se como va a ser el diseño en general pero siento que este encabezado está bien para empezar

---
+++ src/routes/components/Globals/AppHeader.svelte
@@ -0,0 +1,33 @@
+<header class="teamTalkHeader">
+    <img class="appIcon" src="/AppIcon.svg" alt="Icono de la aplicación"/>
+    <h1 class="title">TeamTalk</h1>
+</header>
+
+<style>
+   .teamTalkHeader {
+        top: 0;
+        height: 70px;
+        width: 100%;
+        background-color: #202020;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-weight: bold;
+        font-size: 20pt;
+        padding: 12px;
+        position: fixed;
+   }
+   .title {
+        transition: all 200ms;
+   }
+   .title:hover {
+        text-shadow: 0px 0px 20px white;
+   }
+   .appIcon {
+        width: 50px;
+        height: 50px;
+        margin-right: 8px;
+   } 
+</style>
