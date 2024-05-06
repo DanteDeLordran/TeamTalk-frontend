2af3a0c7d9f2726fc87d703e0e544afee26260c8
DanteDeLordran <lordran@lordran.dev>
2024-05-05 18:09:12 -0600

    Getting token from backend

--- src/routes/components/LoginComponent/LoginCard.svelte
+++ src/routes/components/LoginComponent/LoginCard.svelte
@@ -1,29 +1,32 @@
 <script lang="ts">
-    // Css imports
     import '../../styles/inputs.css'
     import '../../styles/form_texts.css'
 
-    // Svelte modules
     import type LoginDTO from "../../../api/models/LoginDTO";
 
-    // Properties
-    export let onLogin = async (loginEntity: LoginDTO) => {};
-
-    // Data bindings
     let email: string = "";
     let password: string = "";
     
-    function handleSubmit(event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement; }) {
+    const handleLogin = async() => {
         if (email.length > 0 && password.length > 0) {
             const loginData: LoginDTO = {email, password};
-            onLogin(loginData);
+            console.log(loginData)
+            const response = await fetch('http://localhost:8000/users/login',{
+                method: 'POST',
+                body: JSON.stringify(loginData),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+            console.log(await response.status)
+            console.log(await response.json())
         }
     }
 
 </script>
 
 <div>
-    <form class="loginCard" on:submit={handleSubmit}>
+    <form class="loginCard" on:submit={handleLogin}>
         <h2>Inicia sesión</h2>
         <input  type="email" 
                 placeholder="Ingrese su email"
@@ -31,8 +34,7 @@
         <input  type="password"
                 placeholder="Ingrese su contraseña"
                 bind:value={password}/>
-        <button type="submit"
-                class="submitButton">Iniciar Sesión</button>
+        <button type="submit" class="submitButton">Iniciar Sesión</button>
     </form>
     <hr />
     <h2>¿No tienes cuenta?</h2>
