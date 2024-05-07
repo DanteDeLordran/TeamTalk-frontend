ba9691872f22da0c9c6ba961c1afc2756b1ed264
DanteDeLordran <lordran@lordran.dev>
2024-05-07 15:33:42 -0600

    Storing user id in session storage

--- src/routes/components/LoginComponent/LoginCard.svelte
+++ src/routes/components/LoginComponent/LoginCard.svelte
@@ -4,6 +4,7 @@
 
     import type LoginDTO from "../../../api/models/LoginDTO";
     import { goto } from '$app/navigation';
+    import type { User } from '../../../api/models/user';
 
     let email: string = "";
     let password: string = "";
@@ -12,7 +13,6 @@
     const handleLogin = async() => {
         if (email.length > 0 && password.length > 0) {
             const loginData: LoginDTO = {email, password};
-            console.log(loginData)
             const response = await fetch('http://localhost:8000/users/login',{
                 method: 'POST',
                 body: JSON.stringify(loginData),
@@ -21,6 +21,18 @@
                 },
             })
             token = await response.json()
+
+            const userResponse = await fetch('http://localhost:8000/users/authenticate', {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                    token : token
+                },
+            })
+
+            let user : User = await userResponse.json()
+
+            sessionStorage.setItem('user_id', user.id)
             sessionStorage.setItem('token', token)
             goto('/lobby')
         }
